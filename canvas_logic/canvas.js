d5597ac9fe095ba96eed7c9b1b0b4f58e0ecceb6
travis.h.oneill@gmail.com <travis.h.oneill@gmail.com>
2016-10-30 20:21:35 -0700

    polygon animation and motion complete

--- canvas_logic/canvas.js
+++ canvas_logic/canvas.js
@@ -1,4 +1,3 @@
-// import Point from './point_class';
 import Polygon from './polygon_class';
 import * as data from './constants';
 
@@ -6,7 +5,7 @@ function initialize(canvasEl){
   window.addEventListener('resize', resizeCanvas, false);
   data.canvas = canvasEl;
   data.ctx = canvasEl.getContext('2d');
-  // randomPoints(100);
+  // randomPolygons(10);
   randomPolygons(10);
   resizeCanvas();
 }
@@ -16,23 +15,14 @@ function resizeCanvas(event){
   data.canvasWidth = window.innerWidth;
   data.canvas.height = data.canvasHeight;
   data.canvas.width = data.canvasWidth;
-  // drawPoints();
-  drawPolygons();
+  setInterval(drawPolygons, 100);
 }
-
-// function randomPoints(n){
-//   for (var i = 0; i < n; i++){
-//     let x = Math.random();
-//     let y = Math.random();
-//     data.points.push(new Point(x, y));
-//   }
-// }
 
 function randomPolygons(n){
   for (var i = 0; i < n; i++){
     const params = {
-      x: Math.random(),
-      y: Math.random(),
+      x: Math.random() * .9 + .05,
+      y: Math.random() * .9 + .05,
       r: data.randInt(50, 25),
       n: data.randInt(8, 3)
     }
@@ -40,61 +30,12 @@ function randomPolygons(n){
   }
 }
 
-// function drawPoints(){
-//   data.ctx.fillStyle = '#ffffff';
-//   data.points.forEach( point => point.draw(data) );
-// }
-
 function drawPolygons(){
+  data.ctx.fillStyle = '#000000';
+  data.ctx.rect(0, 0, data.canvasWidth, data.canvasHeight);
+  data.ctx.fill();
   data.polygons.forEach( polygon => polygon.draw(data) );
 }
-
-// class Point{
-//   constructor(x, y){
-//     this.x = x;
-//     this.y = y;
-//     this.r = 2
-//     this.vx = 0;
-//     this.vy = 0;
-//   }
-//
-//   draw(){
-//     ctx.beginPath();
-//     ctx.arc(this.x * canvasWidth, this.y * canvasHeight, this.r, 0, inRad(360));
-//     ctx.fill();
-//   }
-//
-//   step(){
-//     this.x += this.vx;
-//     this.y += this.vy;
-//   }
-// }
-//
-// class Polygon{
-//   constructor(x, y, n, r){
-//     this.x = x;
-//     this.y = y;
-//     this.r = 50
-//     this.vx = 0;
-//     this.vy = 0;
-//   }
-//
-//   draw(){
-//     ctx.beginPath();
-//     ctx.arc(this.x * canvasWidth, this.y * canvasHeight, this.r, 0, inRad(360));
-//     ctx.fill();
-//   }
-//
-//   step(){
-//     this.x += this.vx;
-//     this.y += this.vy;
-//   }
-// }
-
-
-
-
-// const inRad = deg => Math.PI * deg / 180
 
 module.exports = {
   initialize: initialize
--- canvas_logic/polygon_class.js
+++ canvas_logic/polygon_class.js
@@ -1,4 +1,5 @@
 import Point from './point_class';
+import { randInt } from './constants';
 
 export default class Polygon{
   constructor({x, y, n, r}){
@@ -6,20 +7,24 @@ export default class Polygon{
     this.y = y;
     this.r = r / 1000;
     this.n = n;
-    this.vx = 0;
-    this.vy = 0;
-    this.points = this.createPoints();
+    this.theta = 0;
+    this.dtheta = randInt(10, 1);
+    this.vx = randInt(5, -5);
+    this.vy = randInt(5, -5);
+    // this.points = this.createPoints();
+    this.tail = [];
   }
 
   createPoints(){
     let points = [];
     const rad = deg => Math.PI * deg / 180;
     const step = 360 / this.n;
-    for (var theta = 0; theta < 360; theta += step) {
+    for (var theta = this.theta; theta < this.theta + 360; theta += step) {
       const x = this.x + (Math.cos(rad(theta)) * this.r);
       const y = this.y + (Math.sin(rad(theta)) * this.r);
       points.push(new Point(x, y));
     }
+    // console.log(this.x, this.y);
     return points;
   }
 
@@ -27,25 +32,51 @@ export default class Polygon{
 
   }
 
-  createSides(ctx, canvasHeight, canvasWidth){
-  }
-
   draw({ctx, inRad, canvasHeight, canvasWidth}){
-    ctx.fillStyle = '#ffffff';
-    ctx.strokeStyle = '#ffffff';
-    let start = this.points[this.points.length - 1];
-    ctx.beginPath();
-    ctx.moveTo(start.x * canvasWidth, start.y * canvasHeight);
-    for (var i = 0; i < this.points.length; i++) {
-      let point = this.points[i];
-      ctx.lineTo(point.x * canvasWidth, point.y * canvasHeight);
+    this.tail.push(this.createPoints());
+    if(this.tail.length > 16){this.tail.shift();}
+    for (var i = 0; i < this.tail.length; i++) {
+      let hex = (i * 17).toString(16);
+      if(i === 0){ hex = '00'; }
+      ctx.strokeStyle = '#' + hex + hex + hex;
+      let shape = this.tail[i];
+      let start = shape[shape.length - 1];
+      ctx.beginPath()
+      ctx.moveTo(start.x * canvasWidth, start.y * canvasHeight);
+      for (var j = 0; j < shape.length; j++) {
+        let point = shape[j];
+        ctx.lineTo(point.x * canvasWidth, point.y * canvasHeight);
+      }
+      ctx.stroke();
     }
-    ctx.stroke();
-
+    this.step(canvasHeight, canvasWidth);
   }
 
-  // step(){
-  //   this.x += this.vx;
-  //   this.y += this.vy;
+  // draw({ctx, inRad, canvasHeight, canvasWidth}){
+  //   this.points = this.createPoints();
+  //   ctx.fillStyle = '#ffffff';
+  //   ctx.strokeStyle = '#ffffff';
+  //   let start = this.points[this.points.length - 1];
+  //   ctx.beginPath();
+  //   ctx.moveTo(start.x * canvasWidth, start.y * canvasHeight);
+  //   for (var i = 0; i < this.points.length; i++) {
+  //     let point = this.points[i];
+  //     ctx.lineTo(point.x * canvasWidth, point.y * canvasHeight);
+  //   }
+  //   ctx.stroke();
+  //   this.step(canvasHeight, canvasWidth);
   // }
+
+  step(canvasHeight, canvasWidth){
+    if(this.x + this.r > 1 || this.x - this.r < 0){
+      this.vx *= -1;
+    }
+    if(this.y + this.r > 1 || this.y - this.r < 0){
+      this.vy *= -1;
+    }
+    this.x += this.vx / canvasWidth;
+    this.y += this.vy / canvasHeight;
+    this.theta = (this.theta + this.dtheta) % 360;
+    console.log(this.vx, this.vy);
+  }
 }

a6a8b7e25fd468fc9d0e3cec7b1b66ecd623f96a
travis.h.oneill@gmail.com <travis.h.oneill@gmail.com>
2016-11-11 22:49:42 -0800

    feat(projects): pause image cycling on hover

--- src/components/projects/index.js
+++ src/components/projects/index.js
@@ -6,7 +6,10 @@ export class ProjectDisplay extends Component {
     super(props);
     this.state = { idx: 0 };
     this.updateImage = this.updateImage.bind(this);
+    this.pause = this.pause.bind(this);
+    this.restart = this.restart.bind(this);
     this.urls = urls[this.props.project];
+    this.paused = false;
   }
 
   componentDidMount(){
@@ -18,15 +21,26 @@ export class ProjectDisplay extends Component {
   }
 
   updateImage(){
+    if(this.paused){ return; }
     let idx = this.state.idx;
     idx++;
     idx %= this.urls.length;
     this.setState({idx: idx});
   }
 
+  pause(event){
+    event.preventDefault();
+    this.paused = true;
+  }
+
+  restart(event){
+    event.preventDefault();
+    this.paused = false;
+  }
+
   render(){
     return(
-      <div className={`project-container ${this.props.project}`}>
+      <div className={`project-container ${this.props.project}`} onMouseOver={this.pause} onMouseOut={this.restart}>
         <div><img className={`project-image ${this.props.project}`} src={this.urls[this.state.idx]} alt='Algo Arena Screenshot' /></div>
       </div>
     );

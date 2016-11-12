4dc532ce819ca4cd6b15ea46b2d95b8299ad94a1
travis.h.oneill@gmail.com <travis.h.oneill@gmail.com>
2016-11-11 18:44:11 -0800

    feat(titlebar): basic functionality added

--- src/components/contact.js
+++ src/components/contact.js
@@ -4,6 +4,9 @@ import React from 'react';
 export const Contact = () => {
   return(
     <div className='contact'>
+      <input type='text' placeholder='Email'/>
+      <input type='name' placeholder='Name'/>
+      <textarea placeholder='Your message...'/>
     </div>
   );
 };
